fdbca6e3d3a20bee4644154018bb1740587f4bab
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-26 13:57:25 +0100

    Support multiple colors for gradiant #4

--- api/lib.py
+++ api/lib.py
@@ -1,12 +1,15 @@
 from PIL import Image
 
-def create_gradient(color1: str, color2: str, width: int, height: int):
-    base = Image.new('RGB', (width, height), color1)
-    top = Image.new('RGB', (width, height), color2)
-    mask = Image.new('L', (width, height))
-    mask_data = []
-    for y in range(height):
-        mask_data.extend([int(255 * (y / height))] * width) # type: ignore
-    mask.putdata(mask_data)
-    base.paste(top, (0, 0), mask)
+
+def create_gradient(mode: str, size: tuple[int, int], colors: list[str]):
+    width, height = size
+    base = Image.new(mode, (width, height), colors[0])
+    for i in range(1, len(colors)):
+        top = Image.new(mode, (width, height), colors[i])
+        mask = Image.new("L", (width, height))
+        mask_data = []
+        for y in range(height):
+            mask_data.extend([int(255 * (y / height))] * width)  # type: ignore
+        mask.putdata(mask_data)
+        base.paste(top, (0, 0), mask)
     return base
