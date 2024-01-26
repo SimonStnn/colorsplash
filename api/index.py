8587efbc35e3dce18dbe000e3a658e03e8ad2b7a
SimonStnn <simon.stijnen.23@gmail.com>
2024-01-26 14:23:41 +0100

    Add support for multiple colors in color image generation #4

--- api/index.py
+++ api/index.py
@@ -2,6 +2,8 @@ import io
 from flask import Flask, request, send_file
 from PIL import Image
 
+from api.lib import create_gradient
+
 app = Flask(__name__)
 
 
@@ -14,18 +16,28 @@ def has_alfa_value(color: str):
 def get_color_image():
     # Get the color parameter from the request
     color = request.args.get("color", "#ffffff").lower()
+    colors = request.args.get("colors")
     # Default to white if no color provided
     if color[0] != "#":
         color = "#" + color
+    if colors:
+        colors = [
+            "#" + color if color[0] != "#" else color for color in colors.split(",")
+        ]
+        colors = [color.lower() for color in colors]
 
     # Get the size parameters from the request
     image_width = int(request.args.get("width", "16"))
     image_height = int(request.args.get("height", "16"))
+    size = (image_width, image_height)
 
     mode = "RGBA" if has_alfa_value(color) else "RGB"
 
-    # Create a solid color image using Pillow (PIL)
-    image = Image.new(mode, (image_width, image_height), color)
+    if colors:
+        image = create_gradient(mode, size, [color, *colors])
+    else:
+        # Create a solid color image using Pillow (PIL)
+        image = Image.new(mode, size, color)
 
     # Save the image to a BytesIO object
     image_io = io.BytesIO()
